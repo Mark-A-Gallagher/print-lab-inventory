55dfdaa50d6831d7d3d97b7facf8393f1b732335
Mark-A-Gallagher <gallaghermark765@gmail.com>
2026-09-05 22:11:09 -0400

    Add model package exports

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,35 @@
 # models/__init__.py
 #
-# TODO: once you've written each model file, import them here so other
-# modules can do `from app.models import Spool` etc. instead of reaching
-# into individual files.
+# DESIGN.md ref: Section 3 (Domain Model)
+#
+# Re-export all SQLAlchemy models from this package so other modules
+# can import them directly:
+#
+#     from app.models import Spool
+#
+# instead of:
+#
+#     from app.models.spool import Spool
+#
+
+from app.models.inventory_event import InventoryEvent, InventoryEventType
+from app.models.machine import Machine
+from app.models.material import Material
+from app.models.print_request import PrintRequest, PrintRequestStatus
+from app.models.reservation_event import (
+    ReservationEvent,
+    ReservationEventType,
+)
+from app.models.spool import Spool
+
+__all__ = [
+    "InventoryEvent",
+    "InventoryEventType",
+    "Machine",
+    "Material",
+    "PrintRequest",
+    "PrintRequestStatus",
+    "ReservationEvent",
+    "ReservationEventType",
+    "Spool",
+]
